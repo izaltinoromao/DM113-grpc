8c016af63989a3b36b0fddcb7050795b1695e5aa
Izaltino <izaltinoromaoneto@hotmail.com>
2024-08-31 17:30:28 -0300

    ✨ New Project: GetMostLengthString

---
+++ grpc_streaming/client/Program.cs
@@ -0,0 +1,59 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Messages;
+using System.Threading.Channels;
+
+namespace client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GrpcChannel channel =
+                GrpcChannel.ForAddress("http://localhost:50005");
+
+            await BiDirectionalStreamingCallAsyncWord(channel);
+        }
+
+        static async Task BiDirectionalStreamingCallAsyncWord(GrpcChannel channel)
+        {
+            var client = new MessageServices.MessageServicesClient(channel);
+            Random random = new Random();
+
+            string[] stringArray =
+            {
+                "Lucas", "Izaltino", "Izal", "jubileudeazeu"
+            };
+
+            //Get request stream
+            var request = client.GetMostLengthString();
+
+            //Listen for responses with thread
+            var serverListenerTask = Task.Run(async () =>
+            {
+                await foreach (var response in
+                    request.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine($"Server --> Client || Max so far: {response.Word}");
+                }
+            });
+
+            //Prepare request
+            foreach (var word in stringArray)
+            {
+                Console.WriteLine(
+                    $"Client --> Server || Sending string: {word}");
+                await request.RequestStream.WriteAsync(
+                    new SingleWordMessage()
+                    {
+                        Word = word
+                    });
+                await Task.Delay(random.Next(1, 3) * 1000);
+            }
+            await request.RequestStream.CompleteAsync();
+            //Complete request preparation
+
+            await serverListenerTask;
+        }
+    }
+}
---
+++ grpc_streaming/server/services/MessageServicesImpl.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.services
+{
+    internal class MessageServicesImpl : MessageServices.MessageServicesBase
+    {
+        public override async Task GetMostLengthString(
+            IAsyncStreamReader<SingleWordMessage> requestStream,
+            IServerStreamWriter<SingleWordMessage> responseStream,
+            ServerCallContext context)
+        {
+            string result = "";
+
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                Console.WriteLine($"String received: {request.Word}");
+                if (request.Word.Length > result.Length)
+                {
+                    result = request.Word;
+                    await responseStream.WriteAsync(
+                        new SingleWordMessage()
+                        {
+                            Word = result
+                        });
+                }
+            }
+        }
+
+    }
+}
